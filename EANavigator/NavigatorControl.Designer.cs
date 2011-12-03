370ffc433380545c9d33d2523e0591f4778ea634
GeertBellekens <geert@bellekens.com>
2011-12-03 17:09:48 +0100

    start refactoring navigator to docked window

---
+++ EANavigator/NavigatorControl.Designer.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wij
+ * Date: 30/11/2011
+ * Time: 4:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TSF.UmlToolingFramework.EANavigator
+{
+	partial class NavigatorControl
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the control.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.NavigatorTree = new System.Windows.Forms.TreeView();
+			this.SuspendLayout();
+			// 
+			// NavigatorTree
+			// 
+			this.NavigatorTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+									| System.Windows.Forms.AnchorStyles.Left) 
+									| System.Windows.Forms.AnchorStyles.Right)));
+			this.NavigatorTree.Location = new System.Drawing.Point(4, 4);
+			this.NavigatorTree.Name = "NavigatorTree";
+			this.NavigatorTree.Size = new System.Drawing.Size(400, 220);
+			this.NavigatorTree.TabIndex = 0;
+			this.NavigatorTree.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.NavigatorTreeBeforeExpand);
+			this.NavigatorTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.NavigatorTreeNodeMouseDoubleClick);
+			// 
+			// NavigatorControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.NavigatorTree);
+			this.Name = "NavigatorControl";
+			this.Size = new System.Drawing.Size(407, 227);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.TreeView NavigatorTree;
+	}
+}
---
+++ EANavigator/NavigatorControl.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using UML=TSF.UmlToolingFramework.UML;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace TSF.UmlToolingFramework.EANavigator
+{
+    [Guid("29fe6e24-9486-4d55-ac16-0200c1b477f2")]
+	[ComVisible(true)]
+	/// <summary>
+	/// Description of NavigatorControl.
+	/// </summary>
+	public partial class NavigatorControl : UserControl
+	{
+		private const string dummyName = "None";
+		public NavigatorControl()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+
+		}
+		/// <summary>
+		/// set the main element to navigate
+		/// </summary>
+		/// <param name="newElement">the element to navigate</param>
+		public void setElement(UML.UMLItem newElement)
+		{
+			this.addElementToTree(newElement,null);
+			
+		}
+
+		private void addElementToTree(UML.UMLItem element,TreeNode parentNode)
+		{
+			//create new node
+			TreeNode elementNode = new TreeNode(element.name);
+			elementNode.Tag = element;
+			
+			//add subnodes
+			foreach (string subNodeName in EAAddin.getMenuOptions(element)) 
+			{
+				TreeNode subNode = new TreeNode(subNodeName.Replace("&",String.Empty));
+				subNode.Nodes.Add( new TreeNode(dummyName));
+				elementNode.Nodes.Add(subNode);
+				
+			}
+			
+			if (parentNode != null)
+			{
+				this.removeDummyNode(parentNode);
+				parentNode.Nodes.Add(elementNode);
+			}
+			else
+			{
+				//no parentNode, add as new root node before any others
+				this.NavigatorTree.Nodes.Insert(0,elementNode);
+				elementNode.Expand();
+				
+			}
+				
+		}
+		void NavigatorTreeBeforeExpand(object sender, TreeViewCancelEventArgs e)
+		{
+			if (BeforeExpand != null)
+				BeforeExpand(sender,e);
+		}
+		private void removeDummyNode(TreeNode node)
+		{
+			foreach ( TreeNode subNode in node.Nodes) 
+			{
+				if (subNode.Text == dummyName)
+				{
+					node.Nodes.Remove(subNode);
+				}
+			}
+		}
+		public event TreeViewCancelEventHandler BeforeExpand;
+		public void setSubNodes(TreeNode node, List<UML.UMLItem> elements)
+		{
+			if (elements.Count > 0)
+			{
+				//remove dummy node
+				node.Nodes.Clear();
+				//add nodes for each named element
+				foreach (UML.UMLItem element in elements) 
+				{
+					this.addElementToTree(element,node);
+				}
+			}
+		}
+		
+		void NavigatorTreeNodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			UML.UMLItem selectedElement = e.Node.Tag as UML.UMLItem;
+			if (selectedElement != null)
+			{
+				selectedElement.open();
+			}
+		}
+	}
+}
