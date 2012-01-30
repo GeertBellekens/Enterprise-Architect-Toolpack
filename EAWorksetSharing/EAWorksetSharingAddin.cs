c32e72de5358344c43148600f8f6da5df29b8334
GeertBellekens <geert@bellekens.com>
2012-01-30 07:39:44 +0100

    show option only in main menu

--- EAWorksetSharing/EAWorksetSharingAddin.cs
+++ EAWorksetSharing/EAWorksetSharingAddin.cs
@@ -12,11 +12,45 @@ namespace EAWorksetSharing
 	{
 		private const string menuName = "&Share Working Sets";
 		private UTF_EA.Model model;
-					
+		
+				
 		public EAWorksetSharingAddin():base()
 		{
 			this.menuHeader = menuName;
 		}
+		/// <summary>
+		/// returns the base implemenation if called from the main menu
+		/// </summary>
+		/// <param name="Repository"></param>
+		/// <param name="MenuLocation"></param>
+		/// <param name="MenuName"></param>
+		/// <returns></returns>
+		public override object EA_GetMenuItems(EA.Repository Repository, string MenuLocation, string MenuName)
+		{
+			//this option will only be shown in the main menu.
+			if (MenuLocation == "MainMenu")
+			{
+				return base.EA_GetMenuItems(Repository,MenuLocation,MenuName);
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+		
+        /// <summary>
+        /// EA_MenuClick events are received by an Add-In in response to user selection of a menu option.
+        /// The event is raised when the user clicks on a particular menu option. When a user clicks on one of your non-parent menu options, your Add-In receives a MenuClick event, defined as follows:
+        /// Sub EA_MenuClick(Repository As EA.Repository, ByVal MenuName As String, ByVal ItemName As String)
+        /// Notice that your code can directly access Enterprise Architect data and UI elements using Repository methods.
+        /// Also look at EA_GetMenuItems.
+        /// </summary>
+        /// <param name="Repository">An EA.Repository object representing the currently open Enterprise Architect model.
+        /// Poll its members to retrieve model data and user interface status information.</param>
+		/// <param name="MenuLocation">String representing the part of the user interface that brought up the menu. 
+        /// Can be TreeView, MainMenu or Diagram.</param>
+        /// <param name="MenuName">The name of the parent menu for which sub-items must be defined. In the case of the top-level menu it is an empty string.</param>
+        /// <param name="ItemName">The name of the option actually clicked, for example, Create a New Invoice.</param>
 		public override void EA_MenuClick(EA.Repository Repository, string MenuLocation, string MenuName, string ItemName)
 		{
 			//initialize model
