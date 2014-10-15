f68eed4a575eb5914d05244d3398c4c2960aab50
GeertBellekens <geert@bellekens.com>
2014-10-15 05:52:08 +0200

    further experimenting with script addin

--- EAScriptAddin/EAScriptAddinAddinClass.cs
+++ EAScriptAddin/EAScriptAddinAddinClass.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using EAAddinFramework.EASpecific;
 using UTF_EA=TSF.UmlToolingFramework.Wrappers.EA;
+using System.Reflection;
 
 namespace EAScriptAddin
 {
@@ -20,10 +21,34 @@ namespace EAScriptAddin
 	{
 		private const string menuMain = "-&Scripting Addin";
 		private const string menuAbout = "&About EA Script Add-in";
+		private UTF_EA.Model model;
+		private List<Script> allScripts {get;set;}
+		private List<ScriptFunction> _allFunctions;
+		private List<ScriptFunction> allFunctions
+		{
+			get
+			{
+				if (this._allFunctions == null)
+				{
+					this._allFunctions = new List<ScriptFunction>();
+					this.allScripts = Script.getAllScripts(model);
+					foreach ( Script script in this.allScripts) 
+					{
+						this._allFunctions.AddRange(script.functions);
+					}
+				}
+				return this._allFunctions;
+			}
+		}
 		public EAScriptAddinAddinClass():base()
 		{
 			this.menuHeader = menuMain;
 			this.menuOptions = new String[] {menuAbout};
+		}
+		public override void EA_FileOpen(EA.Repository Repository)
+		{
+			this.model = new UTF_EA.Model(Repository);
+			
 		}
 		public override void EA_MenuClick(EA.Repository Repository, string MenuLocation, string MenuName, string ItemName)
 		{
@@ -34,10 +59,34 @@ namespace EAScriptAddin
 			}
 		}
 		
+		public override bool EA_OnContextItemDoubleClicked(EA.Repository Repository, string GUID, EA.ObjectType ot)
+		{
+			object[] parameter = new object[]{GUID,ot};
+			bool returnValue = false;
+			MethodBase currentMethod = MethodBase.GetCurrentMethod();
+			ScriptFunction function = this._allFunctions.Find (f => f.name == currentMethod.Name && f.numberOfParameters == parameter.Length);
+			if (function != null)
+			{
+				object result = function.execute(parameter);
+				if (result is bool)
+				{
+					returnValue = (bool)result;
+				}
+			}
+			return returnValue;
+		}
 		void test(EA.Repository repository)
 		{
 			UTF_EA.Model model = new UTF_EA.Model(repository);
 			List<Script> scripts = Script.getAllScripts(model);
+			foreach ( Script script in scripts) 
+			{
+				foreach (ScriptFunction function in script.functions) 
+				{
+					object[] parameter = new object[]{"{D67CC034-D288-4935-B302-2049F5890DBD}"};
+					function.execute(parameter);
+				}	
+			}
 		}
 	}
 }
