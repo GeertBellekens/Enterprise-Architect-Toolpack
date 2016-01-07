b4e6c81c3bcadad89bddee7bd99b28a95e3d4dd0
Geert Bellekens <geert@bellekens.com>
2016-01-07 08:29:27 +0100

    Added ECDMComposerAddin

---
+++ ECDMMessageComposer/ECDMMessageComposerAddin.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Geert
+ * Date: 5/01/2016
+ * Time: 9:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ECDMMessageComposer
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class ECDMMessageComposerAddin : EAAddinFramework.EAAddinBase
+	{
+		/// <summary>
+		/// Tell EA the name of this Schema composer add-in
+		/// </summary>
+		/// <param name="Repository">the repository object</param>
+		/// <param name="displayName">the name that will be displayed</param>
+		/// <returns>true</returns>
+		public override bool EA_IsSchemaExporter(EA.Repository Repository, ref string displayName)
+		{
+			displayName = "ECDM Message Composer";
+			return true;
+		}
+		
+		/// <summary>
+		/// The Add-in can optionally implement this function.
+		/// Using the SchemaProfile interface an Add-in can adjust the capabilities of the Schema Composer. (See Automation Interface)
+		/// </summary>
+		/// <param name="Repository">the repository object</param>
+		/// <param name="profile">the EA SchemaProfile object</param>
+		public override void EA_GetProfileInfo(EA.Repository Repository, EA.SchemaProfile profile)
+		{
+            profile.AddExportFormat("ECDM Message");
+		}
+		
+		/// <summary>
+		/// If a user selects any of the outputs listed by the Add-in, this function will be invoked. 
+		/// The function receives the Schema Composer automation interface, which can be used to traverse the schema.
+		/// </summary>
+		/// <param name="Repository"></param>
+		/// <param name="composer"></param>
+		public override void EA_GenerateFromSchema(EA.Repository Repository, EA.SchemaComposer composer, string exports)
+		{
+			foreach (EA.SchemaType schemaType in getSchemaTypes(composer)) 
+			{
+				EA.ModelType modelType = composer.FindInModel(schemaType.TypeID);
+				//loop the properties
+				foreach (EA.SchemaProperty property in getSchemaProperties(schemaType)) 
+				{
+					string propertyName = property.Name;
+				}
+			} 
+		}
+		
+		private List<EA.SchemaType> getSchemaTypes (EA.SchemaComposer composer)
+		{
+			List<EA.SchemaType> schemaTypes = new List<EA.SchemaType>();
+			EA.SchemaTypeEnum schemaTypeEnumerator = composer.SchemaTypes;
+            EA.SchemaType schemaType = schemaTypeEnumerator.GetFirst();
+            while (schemaType != null)
+            {
+                schemaTypes.Add(schemaType);
+                schemaType = schemaTypeEnumerator.GetNext();
+            }
+            return schemaTypes;
+		}
+		private List<EA.SchemaProperty> getSchemaProperties (EA.SchemaType schemaType)
+		{
+			List<EA.SchemaProperty> schemaProperties = new List<EA.SchemaProperty>();
+			EA.SchemaPropEnum schemaPropEnumerator = schemaType.Properties;
+			EA.SchemaProperty schemaProperty = schemaPropEnumerator.GetFirst();
+			while (	schemaProperty != null)
+			{
+				schemaProperties.Add(schemaProperty);
+				schemaProperty = schemaPropEnumerator.GetNext();
+			}
+			return schemaProperties;
+		}
+	}
+}
