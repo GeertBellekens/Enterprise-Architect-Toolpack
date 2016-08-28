4c1946177f493df23f73c5aee6041c56fddede49
Geert Bellekens <geert@bellekens.com>
2016-08-28 09:52:26 +0200

    refactoring to one listview

--- EADatabaseTransformer/DBCompareControl.Designer.cs
+++ EADatabaseTransformer/DBCompareControl.Designer.cs
@@ -7,18 +7,14 @@ namespace EADatabaseTransformer
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
-		private System.Windows.Forms.SplitContainer splitContainer1;
-		private System.Windows.Forms.ListView originalDBListView;
-		private System.Windows.Forms.ColumnHeader typeColumn;
-		private System.Windows.Forms.ColumnHeader propertiesColumn;
-		private System.Windows.Forms.ListView newDBListView;
-		private System.Windows.Forms.ColumnHeader newTableColumn;
-		private System.Windows.Forms.ColumnHeader newItemTypeColum;
+		private System.Windows.Forms.ListView compareDBListView;
+		private System.Windows.Forms.ColumnHeader tableColumn;
+		private System.Windows.Forms.ColumnHeader itemTypeColum;
 		private System.Windows.Forms.ColumnHeader newPropertiesColumn;
-		private System.Windows.Forms.ColumnHeader tableHeader;
-		private System.Windows.Forms.ColumnHeader nameHeader;
-		private System.Windows.Forms.ColumnHeader nameColumn;
+		private System.Windows.Forms.ColumnHeader newNameColumn;
 		private System.Windows.Forms.ColumnHeader compareStatusColumn;
+		private System.Windows.Forms.ColumnHeader existingNameColumn;
+		private System.Windows.Forms.ColumnHeader existingPropertiesColumn;
 		
 		/// <summary>
 		/// Disposes resources used by the control.
@@ -41,139 +37,80 @@ namespace EADatabaseTransformer
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-			this.newDBListView = new System.Windows.Forms.ListView();
-			this.newTableColumn = new System.Windows.Forms.ColumnHeader();
-			this.newItemTypeColum = new System.Windows.Forms.ColumnHeader();
-			this.nameColumn = new System.Windows.Forms.ColumnHeader();
-			this.newPropertiesColumn = new System.Windows.Forms.ColumnHeader();
-			this.originalDBListView = new System.Windows.Forms.ListView();
-			this.tableHeader = new System.Windows.Forms.ColumnHeader();
-			this.typeColumn = new System.Windows.Forms.ColumnHeader();
-			this.nameHeader = new System.Windows.Forms.ColumnHeader();
-			this.propertiesColumn = new System.Windows.Forms.ColumnHeader();
+			this.compareDBListView = new System.Windows.Forms.ListView();
 			this.compareStatusColumn = new System.Windows.Forms.ColumnHeader();
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-			this.splitContainer1.Panel1.SuspendLayout();
-			this.splitContainer1.Panel2.SuspendLayout();
-			this.splitContainer1.SuspendLayout();
+			this.itemTypeColum = new System.Windows.Forms.ColumnHeader();
+			this.tableColumn = new System.Windows.Forms.ColumnHeader();
+			this.newNameColumn = new System.Windows.Forms.ColumnHeader();
+			this.newPropertiesColumn = new System.Windows.Forms.ColumnHeader();
+			this.existingNameColumn = new System.Windows.Forms.ColumnHeader();
+			this.existingPropertiesColumn = new System.Windows.Forms.ColumnHeader();
 			this.SuspendLayout();
 			// 
-			// splitContainer1
+			// compareDBListView
 			// 
-			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			this.compareDBListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
 			| System.Windows.Forms.AnchorStyles.Left) 
 			| System.Windows.Forms.AnchorStyles.Right)));
-			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-			this.splitContainer1.Name = "splitContainer1";
-			// 
-			// splitContainer1.Panel1
-			// 
-			this.splitContainer1.Panel1.Controls.Add(this.newDBListView);
-			// 
-			// splitContainer1.Panel2
-			// 
-			this.splitContainer1.Panel2.Controls.Add(this.originalDBListView);
-			this.splitContainer1.Size = new System.Drawing.Size(902, 354);
-			this.splitContainer1.SplitterDistance = 475;
-			this.splitContainer1.TabIndex = 0;
-			// 
-			// newDBListView
-			// 
-			this.newDBListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+			this.compareDBListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
 			this.compareStatusColumn,
-			this.newTableColumn,
-			this.newItemTypeColum,
-			this.nameColumn,
-			this.newPropertiesColumn});
-			this.newDBListView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.newDBListView.FullRowSelect = true;
-			this.newDBListView.GridLines = true;
-			this.newDBListView.Location = new System.Drawing.Point(0, 0);
-			this.newDBListView.Name = "newDBListView";
-			this.newDBListView.Size = new System.Drawing.Size(475, 354);
-			this.newDBListView.TabIndex = 1;
-			this.newDBListView.UseCompatibleStateImageBehavior = false;
-			this.newDBListView.View = System.Windows.Forms.View.Details;
-			this.newDBListView.Resize += new System.EventHandler(this.NewDBListViewResize);
-			// 
-			// newTableColumn
-			// 
-			this.newTableColumn.DisplayIndex = 1;
-			this.newTableColumn.Text = "Table";
-			this.newTableColumn.Width = 80;
-			// 
-			// newItemTypeColum
-			// 
-			this.newItemTypeColum.DisplayIndex = 2;
-			this.newItemTypeColum.Text = "Item Type";
-			this.newItemTypeColum.Width = 80;
-			// 
-			// nameColumn
-			// 
-			this.nameColumn.DisplayIndex = 3;
-			this.nameColumn.Text = "Name";
-			this.nameColumn.Width = 120;
-			// 
-			// newPropertiesColumn
-			// 
-			this.newPropertiesColumn.DisplayIndex = 4;
-			this.newPropertiesColumn.Text = "Properties";
-			this.newPropertiesColumn.Width = 115;
-			// 
-			// originalDBListView
-			// 
-			this.originalDBListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-			this.tableHeader,
-			this.typeColumn,
-			this.nameHeader,
-			this.propertiesColumn});
-			this.originalDBListView.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.originalDBListView.FullRowSelect = true;
-			this.originalDBListView.GridLines = true;
-			this.originalDBListView.Location = new System.Drawing.Point(0, 0);
-			this.originalDBListView.Name = "originalDBListView";
-			this.originalDBListView.Size = new System.Drawing.Size(423, 354);
-			this.originalDBListView.TabIndex = 0;
-			this.originalDBListView.UseCompatibleStateImageBehavior = false;
-			this.originalDBListView.View = System.Windows.Forms.View.Details;
-			this.originalDBListView.Resize += new System.EventHandler(this.OriginalDBListViewResize);
-			// 
-			// tableHeader
-			// 
-			this.tableHeader.Text = "Table";
-			this.tableHeader.Width = 80;
-			// 
-			// typeColumn
-			// 
-			this.typeColumn.Text = "Item Type";
-			this.typeColumn.Width = 80;
-			// 
-			// nameHeader
-			// 
-			this.nameHeader.Text = "Name";
-			this.nameHeader.Width = 120;
-			// 
-			// propertiesColumn
-			// 
-			this.propertiesColumn.Text = "Properties";
+			this.itemTypeColum,
+			this.tableColumn,
+			this.newNameColumn,
+			this.newPropertiesColumn,
+			this.existingNameColumn,
+			this.existingPropertiesColumn});
+			this.compareDBListView.FullRowSelect = true;
+			this.compareDBListView.GridLines = true;
+			this.compareDBListView.Location = new System.Drawing.Point(0, 0);
+			this.compareDBListView.Name = "compareDBListView";
+			this.compareDBListView.Size = new System.Drawing.Size(902, 354);
+			this.compareDBListView.TabIndex = 1;
+			this.compareDBListView.UseCompatibleStateImageBehavior = false;
+			this.compareDBListView.View = System.Windows.Forms.View.Details;
+			this.compareDBListView.Resize += new System.EventHandler(this.CompareListViewResize);
 			// 
 			// compareStatusColumn
 			// 
-			this.compareStatusColumn.DisplayIndex = 0;
 			this.compareStatusColumn.Text = "Status";
+			// 
+			// itemTypeColum
+			// 
+			this.itemTypeColum.Text = "Item Type";
+			this.itemTypeColum.Width = 80;
+			// 
+			// tableColumn
+			// 
+			this.tableColumn.Text = "Table";
+			this.tableColumn.Width = 80;
+			// 
+			// newNameColumn
+			// 
+			this.newNameColumn.Text = "New Name";
+			this.newNameColumn.Width = 120;
+			// 
+			// newPropertiesColumn
+			// 
+			this.newPropertiesColumn.Text = "New Properties";
+			this.newPropertiesColumn.Width = 190;
+			// 
+			// existingNameColumn
+			// 
+			this.existingNameColumn.Text = "Existing Name";
+			this.existingNameColumn.Width = 123;
+			// 
+			// existingPropertiesColumn
+			// 
+			this.existingPropertiesColumn.Text = "Existing Properties";
+			this.existingPropertiesColumn.Width = 193;
 			// 
 			// DBCompareControl
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.compareDBListView);
 			this.Name = "DBCompareControl";
-			this.Size = new System.Drawing.Size(902, 423);
-			this.splitContainer1.Panel1.ResumeLayout(false);
-			this.splitContainer1.Panel2.ResumeLayout(false);
-			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-			this.splitContainer1.ResumeLayout(false);
+			this.Size = new System.Drawing.Size(902, 420);
 			this.ResumeLayout(false);
 
 		}
--- EADatabaseTransformer/DBCompareControl.cs
+++ EADatabaseTransformer/DBCompareControl.cs
@@ -13,8 +13,7 @@ namespace EADatabaseTransformer
 	/// </summary>
 	public partial class DBCompareControl : UserControl
 	{
-		private DB.Database _originalDatabase;
-		private DB.Database _newDatabase;
+		DB.Compare.DatabaseComparer _comparer = null;
 		public DBCompareControl()
 		{
 			Application.EnableVisualStyles();	
@@ -24,16 +23,15 @@ namespace EADatabaseTransformer
 			InitializeComponent();
 			
 			//resize columns
-			resizeOriginalDBGridColumns();
-			resizeNewDBGridColumns();
+			resizeCompareGridColumns();
 		}
 		public void clear()
 		{
-			this.newDBListView.Items.Clear();
-			this.originalDBListView.Items.Clear();
+			this.compareDBListView.Items.Clear();
 		}
 		public void loadComparison(DB.Compare.DatabaseComparer comparer)
 		{
+			this._comparer = comparer;
 			this.clear();
 			string tableName = string.Empty;
 			foreach (var comparedItem in comparer.comparedItems) 
@@ -48,68 +46,56 @@ namespace EADatabaseTransformer
 					tableName = comparedItem.existingDatabaseItem.name;
 				}
 				
-				var newItem = new ListViewItem(comparedItem.comparisonStatusName);
-				newItem = addListViewItem(comparedItem.newDatabaseItem, tableName, newItem);
-				this.setStatusColor(newItem,comparedItem);
-				this.newDBListView.Items.Add(newItem);
-				var existingItem =addListViewItem(comparedItem.existingDatabaseItem,tableName,null);
-				this.setStatusColor(existingItem,comparedItem);
-				this.originalDBListView.Items.Add(existingItem);
+				var newItem = addListViewItem(comparedItem,tableName);
+				this.setStatusColor(newItem);
+				this.compareDBListView.Items.Add(newItem);
+
 			}
 		}
-		private void setStatusColor(ListViewItem item, DB.Compare.DatabaseItemComparison comparedItem)
+		private void setStatusColor(ListViewItem item)
 		{
+			var comparedItem = (DB.Compare.DatabaseItemComparison)item.Tag;
 			switch (comparedItem.comparisonStatus) 
 			{
 				case DB.Compare.DatabaseComparisonStatusEnum.changed:
-					item.BackColor = Color.Orange;
-					item.Font = new Font(item.Font,FontStyle.Bold);
+					item.BackColor = Color.FromArgb(204,204,255);//purple-bleu
 					break;
 				case DB.Compare.DatabaseComparisonStatusEnum.newItem:
-					item.BackColor = Color.LightGreen;
-					item.Font = new Font(item.Font,FontStyle.Bold);
+					item.BackColor = Color.FromArgb(204,255,204);//pale green
 					break;
 				case DB.Compare.DatabaseComparisonStatusEnum.deletedItem:
-					item.BackColor = Color.PaleVioletRed;
-					item.Font = new Font(item.Font,FontStyle.Bold);
+					item.BackColor = Color.FromArgb(255,216,216);//pale red
 					break;										
 			}
 		}
-		private ListViewItem addListViewItem(DB.DatabaseItem item,string tableName,ListViewItem listViewItem)
+		private ListViewItem addListViewItem(DB.Compare.DatabaseItemComparison comparison,string tableName)
 		{
-			if (listViewItem == null) 
-			{
-				listViewItem = new ListViewItem(tableName);
-			}
-			else
-			{
-				listViewItem.SubItems.Add(tableName);
-			}
-			if (item != null)
-			{
-				listViewItem.SubItems.Add(item.itemType);
-				listViewItem.SubItems.Add(item.name);
-				listViewItem.SubItems.Add(item.properties);
-			}
-			else
-			{
-				listViewItem.SubItems.Add(string.Empty);
-				listViewItem.SubItems.Add(string.Empty);
-				listViewItem.SubItems.Add(string.Empty);
-			}
-			listViewItem.Tag = item;
+
+			var listViewItem = new ListViewItem(comparison.comparisonStatusName);
+			listViewItem.SubItems.Add(comparison.itemType);
+			listViewItem.SubItems.Add(tableName);
+			
+			addDatabaseItemSpecifics(listViewItem,comparison.newDatabaseItem );
+			addDatabaseItemSpecifics(listViewItem,comparison.existingDatabaseItem);
+			
+			listViewItem.Tag = comparison;
 			return listViewItem;
 		}
-		public void loadOriginalDatabase(DB.Database database)
+		
+		private void addDatabaseItemSpecifics(ListViewItem listViewItem,DB.DatabaseItem databaseItem )
 		{
-			this._originalDatabase = database;
-			loadDatabaseInGrid(_originalDatabase, this.originalDBListView);
+			if (databaseItem != null)
+			{
+				listViewItem.SubItems.Add(databaseItem.name);
+				listViewItem.SubItems.Add(databaseItem.properties);
+			}
+			else
+			{
+				listViewItem.SubItems.Add(string.Empty);
+				listViewItem.SubItems.Add(string.Empty);
+			}
 		}
-		public void loadNewDatabase(DB.Database database)
-		{
-			this._newDatabase = database;
-			loadDatabaseInGrid(_newDatabase, this.newDBListView);
-		}
+		
 		private void loadDatabaseInGrid(DB.Database database, ListView grid)
 		{
 			foreach (var table  in database.tables) 
@@ -139,23 +125,16 @@ namespace EADatabaseTransformer
 			listViewItem.SubItems.Add(item.properties);
 			grid.Items.Add(listViewItem);
 		}
-		private void resizeOriginalDBGridColumns()
+
+		private void resizeCompareGridColumns()
 		{
 			//set the last column to fill
-			this.originalDBListView.Columns[originalDBListView.Columns.Count - 1].Width = -2;
+			this.compareDBListView.Columns[compareDBListView.Columns.Count - 1].Width = -2;
 		}
-		private void resizeNewDBGridColumns()
+		void CompareListViewResize(object sender, EventArgs e)
 		{
-			//set the last column to fill
-			this.newDBListView.Columns[newDBListView.Columns.Count - 1].Width = -2;
+			resizeCompareGridColumns();
 		}
-		void NewDBListViewResize(object sender, EventArgs e)
-		{
-			resizeNewDBGridColumns();
-		}
-		void OriginalDBListViewResize(object sender, EventArgs e)
-		{
-			resizeOriginalDBGridColumns();
-		}
+
 	}
 }
