975719f244eefc8a1b65785f7f32a23edb4e3127
Geert Bellekens <geert@bellekens.com>
2016-11-07 15:35:04 +0100

    added EA Mapping

---
+++ EAMapping/EAMappingAddin.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using UML=TSF.UmlToolingFramework.UML;
+using TSF_EA=TSF.UmlToolingFramework.Wrappers.EA;
+using EAAddinFramework;
+
+namespace EAMapping
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class EAMappingAddin:EAAddinBase
+	{
+		// define menu constants
+        const string menuName = "-&EA Mapping";
+        const string menuMap = "&Show Mapping";
+        const string menuSettings = "&Settings";
+        const string menuAbout = "&About";
+        /// <summary>
+        /// constructor, set menu names
+        /// </summary>
+        public EAMappingAddin():base()
+        {
+        	this.menuHeader = menuName;
+			this.menuOptions = new string[]{menuMap, menuSettings, menuAbout};
+        }
+                /// <summary>
+        /// Called when user makes a selection in the menu.
+        /// This is your main exit point to the rest of your Add-in
+        /// </summary>
+        /// <param name="Repository">the repository</param>
+        /// <param name="Location">the location of the menu</param>
+        /// <param name="MenuName">the name of the menu</param>
+        /// <param name="ItemName">the name of the selected menu item</param>
+        public override void EA_MenuClick(EA.Repository Repository, string Location, string MenuName, string ItemName)
+        {
+            switch (ItemName)
+            {
+                case menuMap:
+                	//TODO
+                    break;
+		        case menuAbout :
+		            new AboutWindow().ShowDialog();
+		            break;
+	            case menuSettings:
+	                //TODO
+	                break;
+            }
+        }
+	}
+}
