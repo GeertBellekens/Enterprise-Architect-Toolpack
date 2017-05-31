2d4b996105c6b3bfe7893b947db2821576b43237
Geert Bellekens <geert@bellekens.com>
2017-05-31 15:35:50 +0200

    Completed SetStructureCorrector

--- MagicdrawMigrator/Correctors/SetStructureCorrector.cs
+++ MagicdrawMigrator/Correctors/SetStructureCorrector.cs
@@ -31,82 +31,85 @@ namespace MagicdrawMigrator
 	                   ,0
 	                  ,LogTypeEnum.log);
 			// Data package aanmaken dat zal gebruikt worden om alles onder te plaatsen
-			TSF_EA.Package newData = this.model.factory.createNewElement<TSF_EA.Package>(mdPackage,"Data");
+			TSF_EA.Package newData = this.model.factory.createNewElement<TSF_EA.Package>(mdPackage,"NewData");
 			newData.save();
-			var importPackage = (TSF_EA.Package)mdPackage.nestedPackages.FirstOrDefault();
-			if (importPackage != null)
-			{
-				foreach (TSF_EA.Package dataPackage in mdPackage.nestedPackages) // Doorloopt de Magicdraw Import
-				{	
-					//tell the user which package we are dealing  with
-					EAOutputLogger.log(this.model,this.outputName
-					                   	,string.Format("{0} Correcting package '{1}' with GUID '{2}'"
-	                                  	,DateTime.Now.ToLongTimeString()
-	                                 	,dataPackage.name
-	                                	,dataPackage.guid)
-	                   ,0
-	                  ,LogTypeEnum.log);
-					if (dataPackage.isEmpty)
+			//reload the new package to make sure we have a complete object
+			newData = (TSF_EA.Package)this.model.getElementWrapperByPackageID(newData.packageID);
+			//loop all subPackages
+			foreach (TSF_EA.Package dataPackage in mdPackage.nestedPackages.Where( x => ! x.Equals(newData))) // Doorloopt de Magicdraw Import
+			{	
+				//tell the user which package we are dealing  with
+				EAOutputLogger.log(this.model,this.outputName
+				                   	,string.Format("{0} Correcting package '{1}' with GUID '{2}'"
+                                  	,DateTime.Now.ToLongTimeString()
+                                 	,dataPackage.name
+                                	,dataPackage.guid)
+                   ,0
+                  ,LogTypeEnum.log);
+
+				foreach (TSF_EA.Package workPackage in dataPackage.nestedPackages)
+				{
+					
+					if (workPackage.isEmpty) 
 					{
-						dataPackage.delete(); //lege packages deleten
+						//workPackage.delete(); //lege packages deleten
 					}
-					else
+					else 
 					{
-						foreach (TSF_EA.Package workPackage in dataPackage.nestedPackages)
+						if(hasPackage(workPackage,newData)) 
 						{
-							
-							if (workPackage.isEmpty) 
-							{
-								workPackage.delete(); //lege packages deleten
-							}
-							else 
-							{
-								if(hasPackage(workPackage,newData)) 
-								{
-								  // inhoud moven naar desbetreffende package
-								  moveContent(workPackage,getPackage(workPackage,newData));
-								}
-								else 
-								{					
-									if(workPackage.name == "AT" || workPackage.name == "BE" || workPackage.name == "CH" || workPackage.name == "DE" || workPackage.name == "DK" || workPackage.name == "NL" || workPackage.name == "NO" || workPackage.name == "SE" || workPackage.name == "SI")
-									{
-										var pack = (TSF_EA.Package)model.getElementWrapperByGUID("{AE324957-30D4-41a6-A507-B31E05EE5EA5}");
-										workPackage.owningPackage = pack;
-										workPackage.save();
-									}
-									else
-									{
-										workPackage.owningPackage = newData;
-										workPackage.save();
-									}
-								}
-								
-							}
-						
+						  // inhoud moven naar desbetreffende package
+						  moveContent(workPackage,getPackage(workPackage,newData));
 						}
-						foreach (TSF_EA.Diagram diagram in dataPackage.ownedDiagrams)
-						{
-							diagram.owningPackage = newData;
-							diagram.save();
+						else 
+						{					
+							if(workPackage.name == "AT" || workPackage.name == "BE" || workPackage.name == "CH" || workPackage.name == "DE" || workPackage.name == "DK" || workPackage.name == "NL" || workPackage.name == "NO" || workPackage.name == "SE" || workPackage.name == "SI")
+							{
+								string getEbixNationalPackageSQL = @"select o.[Object_ID] from (t_object o
+																	inner join [t_objectproperties] tv on o.[Object_ID] = tv.[Object_ID] )
+																	where tv.[Property] = 'md_guid'
+																	and tv.value = '_11_5_5300164_1160397913975_435853_333'";
+																												
+								var ebixNationalPackage = (TSF_EA.Package)model.getElementWrappersByQuery(getEbixNationalPackageSQL).FirstOrDefault();
+								workPackage.owningPackage = ebixNationalPackage;
+								workPackage.save();
+							}
+							else
+							{
+								Logger.log ("parentID before: " +  workPackage.WrappedPackage.ParentID);
+								workPackage.owningPackage = newData;
+								//debug
+								Logger.log ("parentID after: " +  workPackage.WrappedPackage.ParentID);
+								workPackage.save();
+							}
 						}
 						
 					}
 					
-					//Delete the unneeded packages
-					if(dataPackage.guid != newData.guid)
+					
+					foreach (TSF_EA.Diagram diagram in dataPackage.ownedDiagrams)
 					{
-						dataPackage.delete();
+						diagram.owningPackage = newData;
+						diagram.save();
 					}
+					
 				}
-
-				mdPackage.refresh();
-				//Log Finished
-					EAOutputLogger.log(this.model,this.outputName
-	                   ,string.Format("{0} Finished Corrections the Package Structure'"
-	                                  ,DateTime.Now.ToLongTimeString())
-	                   ,0
-	                  ,LogTypeEnum.log);
+				
+				//Delete the unneeded packages
+				if(dataPackage.guid != newData.guid)
+				{
+					dataPackage.delete();
+				}
 			}
+			//refresh the package to show the new structure
+			mdPackage.refresh();
+			//Log Finished
+				EAOutputLogger.log(this.model,this.outputName
+                   ,string.Format("{0} Finished Corrections the Package Structure'"
+                                  ,DateTime.Now.ToLongTimeString())
+                   ,0
+                  ,LogTypeEnum.log);
+			
 		}
 		
 		public void moveContent(TSF_EA.Package subPackage, TSF_EA.Package newData)
