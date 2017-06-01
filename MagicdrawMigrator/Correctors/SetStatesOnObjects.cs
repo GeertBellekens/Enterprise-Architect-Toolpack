cfdf7e9cf48526095bdeb949f970c343700bd023
MatthiasVanDE <matthias.vanderelst@gmail.com>
2017-06-01 12:37:48 +0200

    New class: SetStatesOnObjects

---
+++ MagicdrawMigrator/Correctors/SetStatesOnObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using EAAddinFramework.Utilities;
+using TSF_EA =TSF.UmlToolingFramework.Wrappers.EA;
+using UML = TSF.UmlToolingFramework.UML;
+using System.Diagnostics;
+using System.Xml;
+
+namespace MagicdrawMigrator
+{
+	/// <summary>
+	/// Description of SetStatesOnObjects.
+	/// </summary>
+	public class SetStatesOnObjects:MagicDrawCorrector
+	{
+		public SetStatesOnObjects(MagicDrawReader magicDrawReader, TSF_EA.Model model, TSF_EA.Package mdPackage):base(magicDrawReader,model,mdPackage)
+		{
+		}
+		
+		
+		public override void correct()
+		{
+			// Look for all the classes under an activity
+			
+			// Change the type to Object
+			
+			// Set the state for that Object to the corresponding state in the MagicDrawReader
+		}
+	}
+}
