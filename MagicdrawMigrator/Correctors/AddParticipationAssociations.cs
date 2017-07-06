beca9add11c6b2ca7be07d8d34b1b156d8ddf6fa
MatthiasVanDE <matthias.vanderelst@gmail.com>
2017-07-06 14:36:14 +0200

    New corrector for associations

---
+++ MagicdrawMigrator/Correctors/AddParticipationAssociations.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using EAAddinFramework.Utilities;
+using TSF_EA =TSF.UmlToolingFramework.Wrappers.EA;
+using UML = TSF.UmlToolingFramework.UML;
+using System.Diagnostics;
+using System.Xml;
+
+namespace MagicdrawMigrator
+{
+	/// <summary>
+	/// Check if association between actor and use case are correctly displayed and if the stereotype 
+	/// 'participates' is correctly applied onto the association
+	/// </summary>
+	public class AddParticipationAssociations:MagicDrawCorrector
+	{
+		public AddParticipationAssociations(MagicDrawReader magicDrawReader, TSF_EA.Model model, TSF_EA.Package mdPackage):base(magicDrawReader,model,mdPackage)
+		{
+		}
+		
+		public override void correct()
+		{
+			
+		}
+	}
+}
