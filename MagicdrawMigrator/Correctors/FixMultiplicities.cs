a5b7e8e3d2973cb5bf494c680f2cf874bf243356
MatthiasVanDE <matthias.vanderelst@gmail.com>
2017-08-02 13:14:14 +0200

    fixmultiplicities

---
+++ MagicdrawMigrator/Correctors/FixMultiplicities.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using EAAddinFramework.Utilities;
+using TSF_EA =TSF.UmlToolingFramework.Wrappers.EA;
+using UML = TSF.UmlToolingFramework.UML;
+using System.Diagnostics;
+using System.Xml;
+
+namespace MagicdrawMigrator
+{
+	/// <summary>
+	/// Description of FixMultiplicities.
+	/// </summary>
+	public class FixMultiplicities:MagicDrawCorrector
+	{
+		public FixMultiplicities(MagicDrawReader magicDrawReader, TSF_EA.Model model, TSF_EA.Package mdPackage):base(magicDrawReader,model,mdPackage)
+		{
+		
+		}
+			
+		public override void correct()
+		{
+			EAOutputLogger.log(this.model,this.outputName
+	                   ,string.Format("{0} Starting fix associations'"
+	                                  ,DateTime.Now.ToLongTimeString())
+	                   ,0
+	                   ,LogTypeEnum.log);
+			
+			
+			
+		
+			
+			
+			
+			
+			//Log finished
+			EAOutputLogger.log(this.model,this.outputName
+	                   ,string.Format("{0} Finished fix associations"
+	                                  ,DateTime.Now.ToLongTimeString())
+	                   ,0
+	                  ,LogTypeEnum.log);
+			
+		}
+		
+	}
+}
