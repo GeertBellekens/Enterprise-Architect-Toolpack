52e2863a577b9ea5890213e99a3b47ceaa8f3b5b
MatthiasVanDE <matthias.vanderelst@gmail.com>
2017-08-04 09:53:33 +0200

    new NotesCorrector

---
+++ MagicdrawMigrator/Correctors/NotesCorrector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using EAAddinFramework.Utilities;
+using TSF_EA =TSF.UmlToolingFramework.Wrappers.EA;
+using UML = TSF.UmlToolingFramework.UML;
+using System.Diagnostics;
+using System.Xml;
+
+namespace MagicdrawMigrator
+{
+	/// <summary>
+	/// Description of NotesCorrector.
+	/// </summary>
+	public class NotesCorrector:MagicDrawCorrector
+	{
+		public NotesCorrector(MagicDrawReader magicDrawReader, TSF_EA.Model model, TSF_EA.Package mdPackage):base(magicDrawReader,model,mdPackage)
+		{
+		}
+		
+		
+		public override void correct()
+		{
+				
+		}
+	}
+}
