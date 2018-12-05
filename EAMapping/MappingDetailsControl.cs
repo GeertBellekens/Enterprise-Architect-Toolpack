a8b03b7950879a708100917c1eaa9578573f3471
Geert Bellekens <geert@bellekens.com>
2018-12-05 13:26:04 +0100

    added tooltips

--- EAMapping/MappingDetailsControl.cs
+++ EAMapping/MappingDetailsControl.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MP = MappingFramework;
+using EAAddinFramework.Mapping;
 
 namespace EAMapping
 {
@@ -20,16 +21,21 @@ namespace EAMapping
         private void loadContent()
         {
             this.fromTextBox.Text = this._mapping?.source?.name;
+            this.toolTip.SetToolTip(this.fromTextBox, ((MappingNode)this._mapping?.source)?.getMappingPathExportString());
             this.toTextBox.Text = this.mapping != null && this.mapping.isEmpty ? 
                                     "<none>" 
                                     : this._mapping?.target?.name;
+            this.toolTip.SetToolTip(this.toTextBox, ((MappingNode)this._mapping?.target)?.getMappingPathExportString());
             this.mappingLogicTextBox.Text = this._mapping?.mappingLogicDescription;
+
             enableDisable();
         }
         private void enableDisable()
         {
             //disable delete button when mapping is readonly
-            this.deleteButton.Enabled = this.mapping != null && !this._mapping.isReadOnly;
+            var enable = this.mapping != null && !this._mapping.isReadOnly;
+            this.deleteButton.Enabled = enable;
+            this.mappingLogicTextBox.ReadOnly = !enable;
         }
 
         private MP.Mapping _mapping;
@@ -48,13 +54,12 @@ namespace EAMapping
 
         private void mappingLogicTextBox_TextChanged(object sender, EventArgs e)
         {
-            if (this.mapping != null)
+            if (this.mapping != null
+                && this.mapping.mappingLogicDescription != mappingLogicTextBox.Text)
             {
                 this.mapping.mappingLogicDescription = mappingLogicTextBox.Text;
                 this.mapping.save();
-
             }
-                
         }
         public event EventHandler mappingDeleted;
         private void deleteButton_Click(object sender, EventArgs e)
