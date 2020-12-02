b76e674f9a7be6fc8ff487097bb9770c94db4447
Geert Bellekens <geert@bellekens.com>
2020-12-02 15:29:44 +0100

    version 2.2.0

--- EAImvertor/Properties/AssemblyInfo.cs
+++ EAImvertor/Properties/AssemblyInfo.cs
@@ -24,4 +24,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.1.20.*")]
+[assembly: AssemblyVersion("2.2.0.*")]
