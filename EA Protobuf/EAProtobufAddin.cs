44b22969fabb258fa9ac4f62741cb94602bab09a
Geert Bellekens <geert@bellekens.com>
2024-08-20 13:08:49 +0200

    starting EA Protobuf

---
+++ EA Protobuf/EAProtobufAddin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EAAddinFramework;
+using EA;
+
+namespace EA_Protobuf
+{
+    public class EAProtobufAddin : EAAddinBase
+    {
+        // menu constants
+        const string menuName = "-&EA Protobuf";
+        const string menuGenerate = "&Generate Protobuf";
+        const string menuTransform = "&Transform to Protobuf profile";
+        const string menuSettings = "&Settings";
+        const string menuAbout = "&About EA Protobuf";
+        const string outputName = "EA Protobuf";
+        public EAProtobufAddin() : base()
+        {
+            // Add menu's to the Add-in in EA
+            this.menuHeader = menuName;
+            this.menuOptions = new string[] {
+                                menuGenerate,
+                                menuTransform,
+                                //menuSettings,
+                                menuAbout
+                              };
+        }
+        public override void EA_MenuClick(Repository Repository, string MenuLocation, string MenuName, string ItemName)
+        {
+            switch (ItemName)
+            {
+                case menuGenerate:
+                    this.generateProtobufSchema();
+                    break;
+                //case menuTransform:
+                //    this.transform();
+                //    break;
+                //case menuSettings:
+                //    //TODO
+                //    break;
+                //case menuAbout:
+                //    new AboutWindow().ShowDialog(this.model.mainEAWindow);
+                //    break;
+            }
+        }
+
+        private void generateProtobufSchema()
+        {
+            ProtobufSchema.GenerateProtoFile("c:\\temp\\test.proto");
+        }
+    }
+}
---
+++ EA Protobuf/ProtobufSchema.cs
@@ -0,0 +1,37 @@
+﻿using Google.Protobuf.Reflection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.Compiler;
+using Google.Protobuf;
+
+namespace EA_Protobuf
+{
+    public class ProtobufSchema
+    {
+        public ProtobufSchema() { }
+        public static void GenerateProtoFile(string filePath)
+        {
+
+            // Define message structure
+            var fileDescriptor = new FileDescriptorProto();
+            fileDescriptor.Name = "test.proto";
+            fileDescriptor.Syntax = "proto3";
+            var message = new DescriptorProto();
+            message.Name = "TestMessage";
+            var field1 = new FieldDescriptorProto();
+            field1.Name = "id";
+            field1.Type = FieldDescriptorProto.Types.Type.Int32;
+            //add field to message
+            message.Field.Add(field1);
+            //add message to file
+            fileDescriptor.MessageType.Add(message);
+            fileDescriptor.WriteTo(new FileStream(filePath, FileMode.OpenOrCreate));  
+            var protoString = fileDescriptor.ToString();
+
+        }
+    }
+}
